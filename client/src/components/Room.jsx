4dac85a24b12518815782951f4a3a88077966633
bishal899 <bishalsardar76@gmail.com>
2026-03-06 20:47:37 +0530

    reduce stun/turn server urls

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -39,16 +39,9 @@ function Room({ roomId }) {
                 iceServers: [
                     // STUN servers for NAT detection
                     { urls: ['stun:stun.l.google.com:19302'] },
-                    { urls: ['stun:stun1.l.google.com:19302'] },
-                    { urls: ['stun:stun2.l.google.com:19302'] },
                     // Free TURN server for relay when P2P fails
                     {
                         urls: ['turn:openrelay.metered.ca:80'],
-                        username: 'openrelayproject',
-                        credential: 'openrelayproject'
-                    },
-                    {
-                        urls: ['turn:openrelay.metered.ca:443'],
                         username: 'openrelayproject',
                         credential: 'openrelayproject'
                     }
