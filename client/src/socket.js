39a3fc0b7a81803e5770ba4bdd0ebd485060d364
bishal899 <bishalsardar76@gmail.com>
2026-03-06 12:17:32 +0530

    update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,3 @@
 import { io } from 'socket.io-client'
-const URL =  'http://localhost:4001'
-export const socket = io(URL)
 
-// 'https://movie-watching-app.onrende.com'
+export const socket = io('https://movie-watching-app.onrender.com')
