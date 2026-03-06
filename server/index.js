3765a446ba48c5ca0f0af555cbd73e386e0cd426
Bishal Sardar <132124715+bishal899@users.noreply.github.com>
2026-03-06 10:26:50 +0530

    Update CORS origin settings for socket server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const httpServer = createServer(app)
 // creating socket server
 const io = new Server(httpServer, {
     cors: {
-        origin: 'https://movie-watching-app.vercel.app/'
+        origin: ["http://localhost:5173", "https://movie-watching-app.vercel.app"]
     }
 })
 
